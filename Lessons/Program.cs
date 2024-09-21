8152769b25686b5115ca8d9a53a153168b460fef
Danil Volkov <volkov20032301@yandex.ru>
2024-09-21 07:00:05 +0300

    Добавьте файлы проекта.

---
+++ Lessons/Program.cs
@@ -0,0 +1,71 @@
+﻿
+Lector lector = new Lector();
+
+Student student = new Student();
+
+Room room = new Room();
+
+room.Enter(lector);
+room.Enter(student);
+
+lector.Say(room, "Добрый день!");
+
+
+public interface IListener
+{
+    void Listen(string sound);
+}
+
+public class Room
+{
+    public void Echo(string sound)
+    {
+
+    }
+
+    public void Enter(IListener listener) { }
+}
+
+public class Lector : IListener
+{
+    public IListener Listener
+    {
+        
+    }
+
+    public void Listen(string sound)
+    {
+        //Console.WriteLine(sound);
+    }
+
+    public void Say(Room room, string speech)
+    {
+        room.Echo(speech);
+    }
+}
+
+public class Student : IListener
+{
+    public Lector Lector
+    {
+        
+    }
+
+    public void Listen(string sound)
+    {
+        // нужен объект студента
+    }
+
+    protected void Write(Notebook notebook)
+    {
+        notebook.Write();
+    }
+}
+
+public class Notebook
+{
+    public void Write()
+    {
+        Console.WriteLine("Write in notebook");
+    }
+}
---
+++ Workout/Program.cs
@@ -0,0 +1,91 @@
+﻿Text txt = new Text(
+    new Word("Тестируем"), new Sign(' '),
+    new Word("мою"), new Sign(' '),
+    new Word("архитектуру"), new Sign('!'), new Sign('\n'));
+
+txt.Print(new PrinterDefault());
+
+txt.Print(new PrinterSpecial());
+
+interface IPrinter
+{
+    void Print(Object message);
+}
+
+class PrinterDefault : IPrinter
+{
+    public virtual void Print(Object message)
+    {
+        Console.Write(message);
+    }
+}
+
+interface IPrintable
+{
+    void Print(IPrinter printer);
+}
+
+class Word : IPrintable
+{
+    private string message;
+    public Word(string message)
+    {
+        this.message = message;
+    }
+
+    public void Print(IPrinter printer)
+    {
+        printer.Print(this.message);
+    }
+}
+
+class Sign : IPrintable
+{
+    private char symbol;
+    public Sign(char symbol)
+    {
+        this.symbol = symbol;
+    }
+    public void Print(IPrinter printer)
+    {
+        printer.Print(this.symbol);
+    }
+}
+
+class Text : IPrintable
+{
+    private IPrintable[] printable;
+
+    public Text(params IPrintable[] printable)
+    {
+        this.printable = printable;
+    }
+    public void Print(IPrinter printer)
+    {
+        foreach (var printable in this.printable)
+        {
+            printable.Print(printer);
+        }
+    }
+}
+
+class PrinterSpecial : PrinterDefault
+{
+    public override void Print(Object message)
+    {
+        if (message.GetType() == typeof(string))
+        {
+            base.Print($"({message})");
+        }
+        else
+        {
+            base.Print(message);
+        }
+    }
+}
+
+interface IPrinterDelegate : IPrinter
+{
+
+}
+
